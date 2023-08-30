2ef38474286c8e23b0f5570303c0732878e989c4
Sebastian Widin <contact@sebastianwidin.se>
2023-08-30 13:37:57 +0200

    added Tremor and prettierrc

--- sustainabytes/src/app/page.tsx
+++ sustainabytes/src/app/page.tsx
@@ -1,6 +1,34 @@
+import { Card, DonutChart, Title } from '@tremor/react'
 import Image from 'next/image'
 
 export default function Home() {
+  const cities = [
+    {
+      name: 'New York',
+      sales: 9800,
+    },
+    {
+      name: 'London',
+      sales: 4567,
+    },
+    {
+      name: 'Hong Kong',
+      sales: 3908,
+    },
+    {
+      name: 'San Francisco',
+      sales: 2400,
+    },
+    {
+      name: 'Singapore',
+      sales: 1908,
+    },
+    {
+      name: 'Zurich',
+      sales: 1398,
+    },
+  ]
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -28,17 +56,16 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <Card className="max-w-lg">
+        <Title>Sales</Title>
+        <DonutChart
+          className="mt-6"
+          data={cities}
+          category="sales"
+          index="name"
+          colors={['slate', 'violet', 'indigo', 'rose', 'cyan', 'amber']}
         />
-      </div>
-
+      </Card>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"

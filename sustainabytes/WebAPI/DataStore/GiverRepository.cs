566f053e0cff639e8b2e57f1adee5298a4277cab
Rickard Sandström <rickard.sandstrom@decerno.se>
2023-08-30 17:29:23 +0200

    added controller, GiverRepository and some Dependecy injection

---
+++ sustainabytes/WebAPI/DataStore/GiverRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.DataStore
+{
+    public class GiverRepository
+    {
+        private readonly SustainaBytesDb _dbContext;
+
+        public GiverRepository(SustainaBytesDb dbContext)
+        {
+            _dbContext = dbContext;
+        }   
+
+        public IEnumerable<Giver> GetGivers()
+        {
+            return _dbContext.Giver
+                .AsNoTracking()
+                .ToList();
+        }
+    }
+}
--- sustainabytes/WebAPI/Program.cs
+++ sustainabytes/WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.DataStore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SustainaBytesDb>(opt => opt.UseSqlServer("Server=tcp:add-hack.database.windows.net,1433;Initial Catalog=add-hack-6;Persist Security Info=False;User ID=addhack6;Password=Dontwastefood1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+builder.Services.AddScoped<GiverRepository>();
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 

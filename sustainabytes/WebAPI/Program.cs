4f5c103ba5c20d521923414998e2ec8b59c65fb8
RickSandstrom <rickard.sandstrom@gmail.com>
2023-08-30 21:29:11 +0200

    Removed possible object cycle

--- sustainabytes/WebAPI/Program.cs
+++ sustainabytes/WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using WebAPI;
 using WebAPI.DataStore;
 
@@ -10,10 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 builder.Services.AddDbContext<SustainaBytesDb>(opt => opt.UseSqlServer("Server=tcp:add-hack.database.windows.net,1433;Initial Catalog=add-hack-6;Persist Security Info=False;User ID=addhack6;Password=Dontwastefood1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
 builder.Services.AddScoped<GiversRepository>();
 builder.Services.AddScoped<ReceiversRepository>();
+builder.Services.AddScoped<DrivesRepository>();
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();

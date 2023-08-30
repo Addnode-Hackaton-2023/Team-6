61fb3db8ff4e96130818fc636f168ace473a954e
Rickard Sandström <rickard.sandstrom@decerno.se>
2023-08-30 20:25:52 +0200

    new publish profile for publishing to Azure

---
+++ sustainabytes/WebAPI/DataStore/DrivesRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.Design;
+using WebAPI.Models;
+
+namespace WebAPI.DataStore
+{
+    public class DrivesRepository
+    {
+        private readonly SustainaBytesDb _dbContext;
+
+        public DrivesRepository(SustainaBytesDb dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Drive> GetDrivesByGiver(int giverId, DateTime fromDate, DateTime toDate)
+        {
+            return _dbContext.Drives
+                .Include(x => x.Pickups)
+                    .ThenInclude(x => x.Giver)
+                .Where(x => x.StartTime.Date >= fromDate && x.EndTime.Date >= toDate)
+                .AsNoTracking()
+                .ToList();
+        }
+
+        public IEnumerable<Drive> GetDrivesByReceiver(int receiverId, DateTime fromDate, DateTime toDate)
+        {
+            return _dbContext.Drives
+                .Include(x => x.Pickups)
+                .Include(x => x.Receiver)
+                .Where(x => x.StartTime.Date >= fromDate && x.EndTime.Date >= toDate)
+                .AsNoTracking()
+                .ToList();
+        }
+
+        public IEnumerable<Drive> GetDrivesByCompany(int companyId, DateTime fromDate, DateTime toDate)
+        {
+            return _dbContext.Drives
+                .Include(x => x.Pickups)
+                    .ThenInclude(x => x.Giver)
+                .Where(x => x.StartTime.Date >= fromDate && x.EndTime.Date >= toDate)
+                .AsNoTracking()
+                .ToList();
+        }
+
+        public IEnumerable<Drive> GetDrives(DateTime fromDate, DateTime toDate)
+        {
+            return _dbContext.Drives
+                .Include(x => x.Pickups)
+                .Where(x => x.StartTime.Date >= fromDate && x.EndTime.Date >= toDate)
+                .AsNoTracking()
+                .ToList();
+        }
+
+    }
+}

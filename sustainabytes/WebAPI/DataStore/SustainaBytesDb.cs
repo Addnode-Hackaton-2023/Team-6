931bc49bd407f1c32ff162cab350f67d4072c1f7
Rickard Sandström <rickard.sandstrom@decerno.se>
2023-08-30 17:09:36 +0200

    added dbcontext

---
+++ sustainabytes/WebAPI/DataStore/SustainaBytesDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.DataStore
+{
+    public class SustainaBytesDb : DbContext
+    {
+        public SustainaBytesDb(DbContextOptions<SustainaBytesDb> options)
+        : base(options) { }
+
+        public DbSet<Giver> Giver => Set<Giver>();
+    }
+}
